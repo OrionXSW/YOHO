ccbe7de60ca98ee20e4c04557b2b7bb1d770eeb8
OrionXSW <1395841813@qq.com>
2026-06-03 18:36:55 +0800

    updata:26/6/3下班前更新Python部分

---
+++ Python/Code/Take_a_while.py
@@ -0,0 +1,30 @@
+
+# print
+"""
+r"xxx"可让字符串以原始内容输出，但末尾不能以奇数\结尾，否则解释器会认为你要转义最后一个引号
+
+"""
+# print(r"Hello\n")
+# print(r'hello,"Bart"')
+# print("""
+# Seig Heil!
+# YYYYY
+#       SSSS
+# SSSS
+# """)
+
+# print("Hello,%s,I'm %d" % ("Mike",11))
+
+"""
+逻辑运算
+与 and  或 or 非 not
+"""
+
+# 列表list
+# 增删查改
+table = ["one","please",11,22]
+print(table)
+print(len(table))
+print(len("Hello"))
+
+# 增
---
+++ You Got C/June_W1.md
@@ -0,0 +1,8 @@
+***June 1st Week***
+---
+
+> Todo
+> 1.python basic concept
+> 2.H003
+> 3.basic_componnet
+
